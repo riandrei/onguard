c4965efcf3ffb0bd566fd19582e58492ca748fa2
DowelleDayle <156673102+DowelleDayle@users.noreply.github.com>
2024-12-09 04:57:08 +0000

    added lock feature in profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,52 +1,57 @@
-import styles from '../css/Profile.module.css'
+import React, { useState } from 'react';
+import styles from '../css/Profile.module.css';
 
 import Nav from '../components/Nav';
 
-import Miko from '../assets/Miko.jpg'
+import Miko from '../assets/Miko.jpg';
 
-const Profile = ({handleNavClick, navCount}) => {
-    return(
+const Profile = ({ handleNavClick, navCount }) => {
+    const [isEditable, setIsEditable] = useState(false);
+
+    const handleEditClick = () => {
+        setIsEditable(!isEditable);
+    };
+
+    return (
         <div className={styles.Profile}>
             <Nav />
             <div className={styles.Profile_body}>
                 <div className={styles.Profile_left}>
-                    <img src={Miko} />
-                    <label for="dp">Upload Profile Picture</label>
-                    <input id="dp" type="file" accept="image/*" />
+                    <img src={Miko} alt="Profile" />
+                    <label htmlFor="dp">Upload Profile Picture</label>
+                    <input id="dp" type="file" accept="image/*" disabled={!isEditable} />
                     <button>Log Out</button>
                 </div>
 
                 <div className={styles.Profile_right}>
-
-                <div className={styles.Password}>
+                    <div className={styles.Password}>
                         <span>Personal Details:</span>
                         <div className={styles.Password_inner}>
                             <div className={styles.Password_inner2}>
                                 <label>New password:</label>
-                                <input type="text" />
+                                <input type="text" disabled={!isEditable} />
                             </div>
                             <div className={styles.Password_inner2}>
                                 <label>Confirm New Password:</label>
-                                <input type="text" />
+                                <input type="text" disabled={!isEditable} />
                             </div>
                         </div>
 
                         <div className={styles.Password_inner3}>
                             <label>Address:</label>
-                            <input type="text" />
+                            <input type="text" disabled={!isEditable} />
                         </div>
 
                         <div className={styles.Password_inner}>
                             <div className={styles.Password_inner2}>
                                 <label>New password:</label>
-                                <input type="text" />
+                                <input type="text" disabled={!isEditable} />
                             </div>
                             <div className={styles.Password_inner2}>
                                 <label>Confirm New Password:</label>
-                                <input type="text" />
+                                <input type="text" disabled={!isEditable} />
                             </div>
                         </div>
-
                     </div>
 
                     <div className={styles.Password}>
@@ -54,22 +59,24 @@ const Profile = ({handleNavClick, navCount}) => {
                         <div className={styles.Password_inner}>
                             <div className={styles.Password_inner2}>
                                 <label>New password:</label>
-                                <input type="password" />
+                                <input type="password" disabled={!isEditable} />
                             </div>
                             <div className={styles.Password_inner2}>
                                 <label>Confirm New Password:</label>
-                                <input type="password" />
+                                <input type="password" disabled={!isEditable} />
                             </div>
                         </div>
                     </div>
                     <div className={styles.Save}>
                         <button>SAVE</button>
-                        <button>EDIT</button>
+                        <button onClick={handleEditClick}>
+                            {isEditable ? 'LOCK' : 'EDIT'}
+                        </button>
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Profile;
